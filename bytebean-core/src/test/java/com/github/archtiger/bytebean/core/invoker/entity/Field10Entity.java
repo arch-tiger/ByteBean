71f8706165b138619a645bb2f026e2d8db8cc7e2
zijidelu <32407211@qq.com>
2026-01-19 23:00:58 +0800

    refactor(project): test引入lombok

--- bytebean-core/src/test/java/com/github/archtiger/bytebean/core/invoker/entity/Field10Entity.java
+++ bytebean-core/src/test/java/com/github/archtiger/bytebean/core/invoker/entity/Field10Entity.java
@@ -1,6 +1,8 @@
 package com.github.archtiger.bytebean.core.invoker.entity;
 
 import com.github.archtiger.bytebean.core.support.ByteBeanReflectUtil;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 10个字段的测试实体类
@@ -9,6 +11,8 @@ import com.github.archtiger.bytebean.core.support.ByteBeanReflectUtil;
  * @author ZIJIDELU
  * @datetime 2026/1/15
  */
+@Setter
+@Getter
 public class Field10Entity {
     private Integer field1;
     private Integer field2;
@@ -20,36 +24,6 @@ public class Field10Entity {
     private Integer field8;
     private Integer field9;
     private Integer field10;
-
-    public Integer getField1() { return field1; }
-    public void setField1(Integer field1) { this.field1 = field1; }
-
-    public Integer getField2() { return field2; }
-    public void setField2(Integer field2) { this.field2 = field2; }
-
-    public Integer getField3() { return field3; }
-    public void setField3(Integer field3) { this.field3 = field3; }
-
-    public Integer getField4() { return field4; }
-    public void setField4(Integer field4) { this.field4 = field4; }
-
-    public Integer getField5() { return field5; }
-    public void setField5(Integer field5) { this.field5 = field5; }
-
-    public Integer getField6() { return field6; }
-    public void setField6(Integer field6) { this.field6 = field6; }
-
-    public Integer getField7() { return field7; }
-    public void setField7(Integer field7) { this.field7 = field7; }
-
-    public Integer getField8() { return field8; }
-    public void setField8(Integer field8) { this.field8 = field8; }
-
-    public Integer getField9() { return field9; }
-    public void setField9(Integer field9) { this.field9 = field9; }
-
-    public Integer getField10() { return field10; }
-    public void setField10(Integer field10) { this.field10 = field10; }
 
     public static void main(String[] args) {
         ByteBeanReflectUtil.getMethods(Field10Entity.class).forEach(System.out::println);
--- bytebean-core/src/test/java/com/github/archtiger/bytebean/core/invoker/entity/Field50Entity.java
+++ bytebean-core/src/test/java/com/github/archtiger/bytebean/core/invoker/entity/Field50Entity.java
@@ -1,5 +1,8 @@
 package com.github.archtiger.bytebean.core.invoker.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 拥有50个字段的测试实体类
  * 用于性能测试
@@ -7,8 +10,11 @@ package com.github.archtiger.bytebean.core.invoker.entity;
  * @author ZIJIDELU
  * @datetime 2026/1/15
  */
+@Setter
+@Getter
 public class Field50Entity {
 
+    // Getter and Setter methods
     private Integer field1;
     private Integer field2;
     private Integer field3;
@@ -63,108 +69,6 @@ public class Field50Entity {
     public Field50Entity() {
         // 默认构造函数
     }
-
-    // Getter and Setter methods
-    public Integer getField1() { return field1; }
-    public void setField1(Integer field1) { this.field1 = field1; }
-    public Integer getField2() { return field2; }
-    public void setField2(Integer field2) { this.field2 = field2; }
-    public Integer getField3() { return field3; }
-    public void setField3(Integer field3) { this.field3 = field3; }
-    public Integer getField4() { return field4; }
-    public void setField4(Integer field4) { this.field4 = field4; }
-    public Integer getField5() { return field5; }
-    public void setField5(Integer field5) { this.field5 = field5; }
-    public Integer getField6() { return field6; }
-    public void setField6(Integer field6) { this.field6 = field6; }
-    public Integer getField7() { return field7; }
-    public void setField7(Integer field7) { this.field7 = field7; }
-    public Integer getField8() { return field8; }
-    public void setField8(Integer field8) { this.field8 = field8; }
-    public Integer getField9() { return field9; }
-    public void setField9(Integer field9) { this.field9 = field9; }
-    public Integer getField10() { return field10; }
-    public void setField10(Integer field10) { this.field10 = field10; }
-    public Integer getField11() { return field11; }
-    public void setField11(Integer field11) { this.field11 = field11; }
-    public Integer getField12() { return field12; }
-    public void setField12(Integer field12) { this.field12 = field12; }
-    public Integer getField13() { return field13; }
-    public void setField13(Integer field13) { this.field13 = field13; }
-    public Integer getField14() { return field14; }
-    public void setField14(Integer field14) { this.field14 = field14; }
-    public Integer getField15() { return field15; }
-    public void setField15(Integer field15) { this.field15 = field15; }
-    public Integer getField16() { return field16; }
-    public void setField16(Integer field16) { this.field16 = field16; }
-    public Integer getField17() { return field17; }
-    public void setField17(Integer field17) { this.field17 = field17; }
-    public Integer getField18() { return field18; }
-    public void setField18(Integer field18) { this.field18 = field18; }
-    public Integer getField19() { return field19; }
-    public void setField19(Integer field19) { this.field19 = field19; }
-    public Integer getField20() { return field20; }
-    public void setField20(Integer field20) { this.field20 = field20; }
-    public Integer getField21() { return field21; }
-    public void setField21(Integer field21) { this.field21 = field21; }
-    public Integer getField22() { return field22; }
-    public void setField22(Integer field22) { this.field22 = field22; }
-    public Integer getField23() { return field23; }
-    public void setField23(Integer field23) { this.field23 = field23; }
-    public Integer getField24() { return field24; }
-    public void setField24(Integer field24) { this.field24 = field24; }
-    public Integer getField25() { return field25; }
-    public void setField25(Integer field25) { this.field25 = field25; }
-    public Integer getField26() { return field26; }
-    public void setField26(Integer field26) { this.field26 = field26; }
-    public Integer getField27() { return field27; }
-    public void setField27(Integer field27) { this.field27 = field27; }
-    public Integer getField28() { return field28; }
-    public void setField28(Integer field28) { this.field28 = field28; }
-    public Integer getField29() { return field29; }
-    public void setField29(Integer field29) { this.field29 = field29; }
-    public Integer getField30() { return field30; }
-    public void setField30(Integer field30) { this.field30 = field30; }
-    public Integer getField31() { return field31; }
-    public void setField31(Integer field31) { this.field31 = field31; }
-    public Integer getField32() { return field32; }
-    public void setField32(Integer field32) { this.field32 = field32; }
-    public Integer getField33() { return field33; }
-    public void setField33(Integer field33) { this.field33 = field33; }
-    public Integer getField34() { return field34; }
-    public void setField34(Integer field34) { this.field34 = field34; }
-    public Integer getField35() { return field35; }
-    public void setField35(Integer field35) { this.field35 = field35; }
-    public Integer getField36() { return field36; }
-    public void setField36(Integer field36) { this.field36 = field36; }
-    public Integer getField37() { return field37; }
-    public void setField37(Integer field37) { this.field37 = field37; }
-    public Integer getField38() { return field38; }
-    public void setField38(Integer field38) { this.field38 = field38; }
-    public Integer getField39() { return field39; }
-    public void setField39(Integer field39) { this.field39 = field39; }
-    public Integer getField40() { return field40; }
-    public void setField40(Integer field40) { this.field40 = field40; }
-    public Integer getField41() { return field41; }
-    public void setField41(Integer field41) { this.field41 = field41; }
-    public Integer getField42() { return field42; }
-    public void setField42(Integer field42) { this.field42 = field42; }
-    public Integer getField43() { return field43; }
-    public void setField43(Integer field43) { this.field43 = field43; }
-    public Integer getField44() { return field44; }
-    public void setField44(Integer field44) { this.field44 = field44; }
-    public Integer getField45() { return field45; }
-    public void setField45(Integer field45) { this.field45 = field45; }
-    public Integer getField46() { return field46; }
-    public void setField46(Integer field46) { this.field46 = field46; }
-    public Integer getField47() { return field47; }
-    public void setField47(Integer field47) { this.field47 = field47; }
-    public Integer getField48() { return field48; }
-    public void setField48(Integer field48) { this.field48 = field48; }
-    public Integer getField49() { return field49; }
-    public void setField49(Integer field49) { this.field49 = field49; }
-    public Integer getField50() { return field50; }
-    public void setField50(Integer field50) { this.field50 = field50; }
 
     // 5参数setter方法 - 用于多参数性能测试
     public void setFiveFields(Integer f1, Integer f2, Integer f3, Integer f4, Integer f5) {
